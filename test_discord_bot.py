ad1ca54436e980b5656403c90f12677297e5d954
Giovanni Grieco <59712930+ChuckHendersen@users.noreply.github.com>
2023-10-17 19:49:19 +0200

    Update test_discord_bot.py

--- test_discord_bot.py
+++ test_discord_bot.py
@@ -1,6 +1,1 @@
-from discord_bot import DiscordBot
 
-print('Sending message')
-bot = DiscordBot('%', 'first message sent from raspberry!')
-bot.run('MTE0NjUxMjAzMDQ1NTU2MjM1MA.GqDzzj.l7GEPGh0379UWtZ5Zh3So_YnBNN-694BHPSWc8')
-print('Message sent')
